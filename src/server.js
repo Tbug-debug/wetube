f48f50ec119a73a0209b6c504e91e168ddb00ee2
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-01-29 15:39:55 +0900

    wetube

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("Fuck you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Fuck you login");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 const serverListening = () =>
   console.log(`Sever listening on port http://localhost:${PORT}`);
 
