653a8be30d18b9177932a76ecd212e42b4790d3e
Tbug-debug <lhg6325@gmail.com>
2022-02-02 14:08:26 +0900

    wetube

---
+++ src/controllers/userController.js
@@ -0,0 +1,3 @@
+export const join = (req, res) => res.send("join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const trending = (req, res) => res.send("Homepage Videos");
+export const watch = (req, res) => res.send("Watch");
+export const edit = (req, res) => res.send("Edit");
