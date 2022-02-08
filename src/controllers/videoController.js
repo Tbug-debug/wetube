c82f3b45b9c9033a16feda24d79e8a0e6125dc72
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-02-08 13:00:22 +0900

    wetub

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,39 @@
+let videos = [
+  {
+    title: "Frist Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 10,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    view: 59,
+    id: 2,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Frist Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      view: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      view: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      view: 59,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  //const id  = req.params.id: 구버전 JavaScript / 위에 것은 ES6 버전.
+  const video = videos[id];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
