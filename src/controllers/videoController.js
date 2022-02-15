06cd440b6dc45d2410e12953fb77349f7a80fd0a
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-02-15 14:27:03 +0900

    wetube

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,7 +7,7 @@ callback 구 버전
 */
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 //promise 신버전
@@ -42,9 +42,7 @@ export const postEdit = async (req, res) => {
     //findByIdAndUpdate는 id 값을 인자로 받는다.
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -59,7 +57,7 @@ export const postUpload = async (req, res) => {
     const video = new Video({
       title: title,
       description: description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     await video.save();
     return res.redirect("/");
@@ -69,4 +67,18 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -11,11 +11,12 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-videoSchema.pre("save", async function () {
-  this.hashtags = this.hashtags[0]
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
+//정적 메소드로 import 없이도 Model.function()형태로 사용가능하게 만듬.
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
