7ae651657d721512bd7481838473c6433cae89cc
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-03-01 14:35:35 +0900

    wetube

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,34 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  //MediaRecorder한테 stream의 정보를 넘겨준다.
+  recorder.ondataavailable = (e) => {
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 200, height: 200 },
   });
@@ -11,5 +37,7 @@ const handleStart = async () => {
   //srcObject는 video가 가질 수 있는 혹은 주는 무엇인가를 의미한다.
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
