937e78ee638458cf86bef07087d4e1280386928e
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-03-01 18:33:32 +0900

    wetube

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,29 +2,31 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   //MediaRecorder한테 stream의 정보를 넘겨준다.
-  recorder.ondataavailable = (e) => {
-    console.log(e);
-    console.log(e.data);
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
-  console.log(recorder);
   recorder.start();
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
