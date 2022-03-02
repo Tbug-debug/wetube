6d6e9c003809c6f1fa15bd9d63a3483d89ddbb90
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-03-02 09:56:19 +0900

    wetube

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,16 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "My recording.webm";
+  // a 링크에 다운로드 property를 추가였다.
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -17,12 +25,17 @@ const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
+  //여기서는 EventLinstener를 추가,제거 하고 있다.
   recorder = new MediaRecorder(stream);
   //MediaRecorder한테 stream의 정보를 넘겨준다.
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
+    //ondataavailable은 녹화가 멈추면 발생되는 event이다.
+    //여기 event는 유저가 video 파일을 다운로드 받을 수 있게끔 도와주는 event이다.
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
+    // 미리보기를 없앴다.
     video.src = videoFile;
+    // 미리보기 대신에 자신이 찍은 영상을 보여주게 만들었다.
     video.loop = true;
     video.play();
   };
@@ -31,6 +44,7 @@ const handleStart = () => {
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
+    //마이크나 카메라와 같은 미디어 장비들에 접근을 시켜준다.
     audio: false,
     video: { width: 200, height: 200 },
   });
