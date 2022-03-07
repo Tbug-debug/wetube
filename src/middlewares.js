e38f78cf227fd491894e3d0bcfa59aba9bd0a571
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-03-07 18:45:08 +0900

    wetube

--- src/middlewares.js
+++ src/middlewares.js
@@ -11,7 +11,7 @@ const s3 = new aws.S3({
 
 const multerUploader = multerS3({
   s3: s3,
-  bucket: "BorderUploader",
+  bucket: "mybuvok",
 });
 
 export const localMiddleware = (req, res, next) => {
